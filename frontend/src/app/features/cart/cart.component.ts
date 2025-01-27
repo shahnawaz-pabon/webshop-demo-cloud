8cb119400c0778af312c9f7d0a6c01baed5bbb4b
shahnawaz-pabon <s.pabon93@gmail.com>
2025-01-27 23:38:46 +0100

    products that added to cart displayed with pricing, styled cart details. Ref #7

--- frontend/src/app/features/cart/cart.component.ts
+++ frontend/src/app/features/cart/cart.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-cart',
-    standalone: true,
-    template: '<p>Cart Works!</p>'
+  selector: 'app-cart',
+  standalone: true,
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+  imports: [CommonModule], // Import CommonModule for ngFor, ngIf, etc.
 })
-export class CartComponent { } 
+export class CartComponent implements OnInit {
+  cart: any[] = [];
+  totalPrice: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    // Fetch the cart data from the service
+    this.cartService.getCart().subscribe((data) => {
+      this.cart = data.cart;
+      this.totalPrice = data.totalPrice;
+    });
+  }
+
+  handleBuyProducts(): void {
+    alert('Redirecting to Stripe Checkout...');
+  }
+}
