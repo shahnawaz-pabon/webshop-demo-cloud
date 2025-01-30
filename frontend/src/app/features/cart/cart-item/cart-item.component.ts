b3f002e35e636d278e2c7154bab5e5ea6be2b0f9
prnb22 <pronabcse22@gmail.com>
2025-01-30 21:53:28 +0100

    Implement cart item view

--- frontend/src/app/features/cart/cart-item/cart-item.component.ts
+++ frontend/src/app/features/cart/cart-item/cart-item.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AppStateService } from '../../../services/app-state.service';
+import { RestService } from '../../../services/rest.service';
+import { CartItem } from '../../../model/cart-item.model';
+import { FormatPricePipe } from '../../../pipes/format-price.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-item',
-  standalone: true,
-  imports: [],
   templateUrl: './cart-item.component.html',
-  styleUrl: './cart-item.component.css'
+  styleUrls: ['./cart-item.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormatPricePipe,
+    RouterLink
+  ]
 })
-export class CartItemComponent {
+export class CartItemComponent implements OnDestroy {
 
+  @Input()
+  set cartItem(value: CartItem) {
+    console.log('CartItem received:', value);
+    this._cartItem = value;
+  }
+  get cartItem(): CartItem {
+    return this._cartItem!;
+  }
+  private _cartItem!: CartItem;
+
+  @ViewChild('quantityRef', {static: true}) quantityRef!: ElementRef;
+
+  @ViewChild('cartItemRef', {static: true}) cartItemRef!: ElementRef;
+
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private restService: RestService,
+    private appState: AppStateService
+    ) { }
+
+  saveCartItem(increment: string) {
+    console.log('Update cart item pressed');
+  }
+
+  deleteCartItem() {
+    console.log('Delete cart item pressed');
+  }
+
+  ngOnDestroy(): void {
+      this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 }
--- frontend/src/app/features/cart/cart.component.ts
+++ frontend/src/app/features/cart/cart.component.ts
@@ -3,18 +3,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
 import { loadStripe, Stripe } from '@stripe/stripe-js';
-import { env } from '../../../environments/env';
+import { environment as env } from '../../../environments/env';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { CartItem } from '../../model/cart-item.model';
+import { Product } from '../../model/product.model';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
-  imports: [CommonModule], // Import CommonModule for ngFor, ngIf, etc.
+  imports: [CommonModule, CartItemComponent],
 })
 export class CartComponent implements OnInit {
-  cart: any[] = [];
+  cart: CartItem[] = [];
   totalPrice: number = 0;
+  totalLength: number = 0;
   private stripePromise: Promise<Stripe | null>;
   private stripePublicKey = env.STRIPE_PUBLIC_KEY;
   private stripeSecretKey = env.STRIPE_SECRET_KEY;
@@ -30,8 +34,23 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
     // Fetch the cart data from the service
     this.cartService.getCart().subscribe((data) => {
-      this.cart = data.cart;
+      this.cart = data.cart.map((item: { product: any, quantity: number, totalPrice: number }) => 
+        new CartItem(
+          new Product(
+            item.product.productId,
+            item.product.title,
+            item.product.summary,
+            item.product.price,
+            item.product.description,
+            item.product.imageUrl
+          ),
+          item.quantity,
+          item.totalPrice
+        )
+      );
       this.totalPrice = data.totalPrice;
+      this.totalLength = data.totalLength;
+      console.log('Cart data:', this.cart);
     });
 
     // Handle payment success/cancel scenarios
@@ -56,10 +75,10 @@ export class CartComponent implements OnInit {
     const lineItems: Record<string, string>[] = this.cart.map((item, index) => {
       return {
         [`line_items[${index}][price_data][currency]`]: 'usd',
-        [`line_items[${index}][price_data][product_data][name]`]: item.product.name,
-        [`line_items[${index}][price_data][product_data][images][0]`]: item.product.imageUrl || '',
-        [`line_items[${index}][price_data][unit_amount]`]: Math.round(item.product.price * 100), // Convert to cents
-        [`line_items[${index}][quantity]`]: item.quantity,
+        [`line_items[${index}][price_data][product_data][name]`]: item.getProduct().getTitle(),
+        [`line_items[${index}][price_data][product_data][images][0]`]: item.getProduct().getImageUrl() || '',
+        [`line_items[${index}][price_data][unit_amount]`]: Math.round(item.getProduct().getPrice() * 100).toString(), // Convert to string
+        [`line_items[${index}][quantity]`]: item.getQuantity().toString(),
       };
     });
   
