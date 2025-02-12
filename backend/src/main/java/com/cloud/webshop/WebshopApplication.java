89c2b60e445133c7c89828af5db2f2ad8c29edd2
shahnawaz-pabon <s.pabon93@gmail.com>
2025-02-12 23:08:14 +0100

    dockerfile optimized, .env loader added for running locally, cors issue solved. Ref #18

--- backend/src/main/java/com/cloud/webshop/WebshopApplication.java
+++ backend/src/main/java/com/cloud/webshop/WebshopApplication.java
@@ -2,11 +2,14 @@ package com.cloud.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.cloud.webshop.config.DotenvInitializer;
 
 @SpringBootApplication
 public class WebshopApplication {
 
 	public static void main(String[] args) {
+		// load .env file
+		DotenvInitializer.loadDotenv();
 		SpringApplication.run(WebshopApplication.class, args);
 	}
 
