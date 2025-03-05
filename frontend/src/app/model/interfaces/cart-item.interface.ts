877c3df4659546ef1e71da7ff4c0c2571edb0561
Juthisarker <tanudas71.21@gmail.com>
2025-03-05 18:24:26 +0100

    when stock level is zero disable the add add cart button

--- frontend/src/app/model/interfaces/cart-item.interface.ts
+++ frontend/src/app/model/interfaces/cart-item.interface.ts
@@ -2,7 +2,6 @@ import { BaseResponse } from './base-response.interface';
 import { Product } from './product.interface';
 
 export interface CartItem {
-    cartId: number;
     product: Product;
     quantity: number;
     totalPrice: number;
