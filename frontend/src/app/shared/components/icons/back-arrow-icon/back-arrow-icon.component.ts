48fd16d7683170a3bcbb16114bd7a11f46caab43
prnb22 <pronabcse22@gmail.com>
2025-03-05 22:33:44 +0100

    change back arrow icon

--- frontend/src/app/shared/components/icons/back-arrow-icon/back-arrow-icon.component.ts
+++ frontend/src/app/shared/components/icons/back-arrow-icon/back-arrow-icon.component.ts
@@ -4,9 +4,17 @@ import { Component } from '@angular/core';
     selector: 'app-back-arrow-icon',
     standalone: true,
     template: `
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
-        </svg>
-    `
+        <span class="arrow">↩</span>
+    `,
+    styles: [`
+        .arrow {
+            font-size: 24px;
+            color: var(--color-primary-100);
+            line-height: 1;
+            display: flex;
+            align-items: center;
+            transform: scaleY(-1);
+        }
+    `]
 })
 export class BackArrowIconComponent { } 
